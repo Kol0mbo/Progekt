80ffd72280fb3f45390b088b0e7f2adb2fcca7f8
Yonchii <58008244+Yonchii@users.noreply.github.com>
2020-05-24 00:45:58 +0300

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -2,8 +2,29 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Wars of Heroes</title>
+	<title>Dream Team</title>
 	<link rel="stylesheet" href="../assets/css/style.css">
 	<link rel="stylesheet" href="../assets/css/style1.css">
 </head>
 <body>
+<div class="container">
+ 	
+<div class="main_content">
+		<nav>
+			<ul class="topmenu">
+			<li><a href="" class="down">Game</a>
+			<ul class="submenu">
+			<li><a href="">CS:GO</a></li>
+			<li><a href="">Dota 2</a></li>
+			<li><a href="">Fortnight</a></li>
+			<li><a href="">PUBG</a></li>
+			</ul>
+			</li>
+		</nav>
+		
+ 	<a href="includes/regis.php" class="buttons"> Зареєстуватися </a>
+ 	<a href="includes/log.php" class="buttons"> Войти </a>
+	 
+</div>
+</div>
+
