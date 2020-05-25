397ca9cb0ceb9538e79b25a6abbd7e1e37e7564a
Kol0mbo <48684830+Kol0mbo@users.noreply.github.com>
2020-05-25 13:07:56 +0300

    додав

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <? include_once 'includes/header.php'; ?>
 <? include_once ('includes/bd.php'); ?>
+<a href="regis.php" class="buttons"> Зареєстуватися </a>
+ <a href="log.php" class="buttons"> Войти </a>
+ </div>
+</div>
 <? include_once 'includes/footer.php'; ?>
 	
