53f0ac8f6f72a260a61ce23f436920adf3c654a5
Kol0mbo <48684830+Kol0mbo@users.noreply.github.com>
2020-05-25 17:41:49 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,81 @@
+<? include_once 'includes/header.php'; ?>
+<? include_once ('includes/bd.php'); ?>
+<a href="includes/regis.php" class="buttons"> Зареєстуватися </a>
+ <a href="includes/log.php" class="buttons"> Войти </a>
+ </div>
+</div>
+<section id="cd-timeline" class="cd-container">
+		<div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-picture">
+				<img src="../assets/image/cd-icon-picture.svg" alt="Picture">
+			</div> 
+
+			<div class="cd-timeline-content">
+				<a href="https://kanobu.ru/news/vsteam-soskidkami-otdayut-igryi-serii-tomb-raider-424473/"> <h2>В Steam со скидками отдают игры серии Tomb Raider</h2></a>
+				<br>
+				<a href="https://kanobu.ru/news/vsteam-soskidkami-otdayut-igryi-serii-tomb-raider-424473/"><img src="https://i19.kanobu.ru/r/900dcb6ae7b7e9a1dfbf472eb8ed4340/1040x-/u.kanobu.ru/editor/images/40/437bdbff-706e-4663-8306-880ff8086a53.jpg"></a>
+			</div> 
+		</div> 
+
+		<div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-movie">
+				<img src="../assets/image/cd-icon-picture.svg">
+			</div> 
+
+			<div class="cd-timeline-content">
+				<a href="https://kanobu.ru/news/vset-utekli-syuzhetnyie-spojleryi-buduschih-sezonov-the-division2-424466/"><h2>В сеть утекли сюжетные спойлеры будущих сезонов The Division 2</h2></a>
+				<br>
+				<a href="https://kanobu.ru/news/vset-utekli-syuzhetnyie-spojleryi-buduschih-sezonov-the-division2-424466/"><img src="https://cdn.igromania.ru/mnt/news/2/c/f/9/f/a/65430/fab712d1320e8372_848x477.jpg"></a>
+			</div> 
+		</div> 
+
+		<div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-picture">
+				<img src="../assets/image/cd-icon-picture.svg">
+			</div> 
+
+			<div class="cd-timeline-content">
+				<a href="https://kanobu.ru/news/pervaya-assassins-creed-pokazalas-skuchnoj-syinu-glavyi-ubisoft-poetomu-vigru-dobavili-sajd-kvestyi-424450/"> <h2>Первая Assassinʼs Creed показалась скучной сыну главы Ubisoft — поэтому в игру добавили сайд-квесты</h2></a>
+				<br>
+				<a href="https://kanobu.ru/news/pervaya-assassins-creed-pokazalas-skuchnoj-syinu-glavyi-ubisoft-poetomu-vigru-dobavili-sajd-kvestyi-424450/"><img src="https://i01.kanobu.ru/r/06135dd6a63d6ea46931fd688c1ad2ac/800x450/u.kanobu.ru/articles/pics/d9e0699f-31cb-4d5f-8fa8-829b16e5807f.jpg"></a>
+				
+			</div> 
+		</div> 
+
+		<div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-location">
+				<img src="../assets/image/cd-icon-picture.svg">
+			</div> 
+
+			<div class="cd-timeline-content">
+				<a href="https://kanobu.ru/news/vworld-oftanks-poyavitsya-berlin-vremen-kontsa-vtoroj-mirovoj-vojnyi-424490/"> <h2>В World of Tanks появится Берлин времен конца Второй мировой войны</h2></a>
+				<br>
+				<a href="https://kanobu.ru/news/vworld-oftanks-poyavitsya-berlin-vremen-kontsa-vtoroj-mirovoj-vojnyi-424490/"><img src="https://regnum.ru/uploads/pictures/news/2020/05/05/regnum_picture_1588674738546793_normal.jpg"></a>
+			</div> 
+		</div> 
+
+		<div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-location">
+				<img src="../assets/image/cd-icon-picture.svg">
+			</div> 
+
+			<div class="cd-timeline-content">
+				<a href="https://shazoo.ru/2020/05/25/94734/krasochnyj-ulichnyj-futbol-v-pervom-trejlere-street-power-football"> <h2>Красочный уличный футбол в первом трейлере Street Power Football</h2></a>
+				<br>
+				<a href="https://shazoo.ru/2020/05/25/94734/krasochnyj-ulichnyj-futbol-v-pervom-trejlere-street-power-football"><img src="https://cdn.shazoo.ru/434437_TcLxO5PjUL_football.jpg"></a>
+			</div> 
+		</div> 
+
+		<div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-movie">
+				<img src="../assets/image/cd-icon-picture.svg">
+			</div>
+			<div class="cd-timeline-content">
+				<a href="https://shazoo.ru/2020/05/25/94732/uluchshennye-npc-i-ekipirovka-geralta-v-rolike-novoj-versii-moda-hd-reworked-project-dlya-the-witcher-3"> <h2>Улучшенные NPC и экипировка Геральта в ролике новой версии мода HD Reworked Project для The Witcher 3</h2></a>
+				<br>
+				<a href="https://shazoo.ru/2020/05/25/94732/uluchshennye-npc-i-ekipirovka-geralta-v-rolike-novoj-versii-moda-hd-reworked-project-dlya-the-witcher-3"><img src="https://cdn.shazoo.ru/434414_XW7RL77teA_geralt.jpg"></a>
+			</div> 
+		</div> 
+	</section> 
+<? include_once 'includes/footer.php'; ?>
+	
