4bdeff936fc6947ea91830ce2bbffbf1085d64ca
Kol0mbo <48684830+Kol0mbo@users.noreply.github.com>
2020-05-25 17:40:01 +0300

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<? include_once 'includes/header.php'; ?>
-<? include_once ('includes/bd.php'); ?>
-<a href="includes/regis.php" class="buttons"> Зареєстуватися </a>
- <a href="includes/log.php" class="buttons"> Войти </a>
- </div>
-</div>
-<? include_once 'includes/footer.php'; ?>
-	
