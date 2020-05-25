bea5ef41b16e9ecf5bb74e7b44df8c9daa9abbdf
Kol0mbo <48684830+Kol0mbo@users.noreply.github.com>
2020-05-25 13:10:40 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <? include_once 'includes/header.php'; ?>
 <? include_once ('includes/bd.php'); ?>
-<a href="regis.php" class="buttons"> Зареєстуватися </a>
- <a href="log.php" class="buttons"> Войти </a>
+<a href="includes/regis.php" class="buttons"> Зареєстуватися </a>
+ <a href="includes/log.php" class="buttons"> Войти </a>
  </div>
 </div>
 <? include_once 'includes/footer.php'; ?>
